15c49cc8de3808a82ba1055e18bbfe4963e727f9
BAOZUN\CSH10105 <1062281158@qq.com>
2018-03-23 18:38:43 +0800

    添加查询所有商品接口

--- src/main/java/com/imooc/bootsell/controller/SellerProductController.java
+++ src/main/java/com/imooc/bootsell/controller/SellerProductController.java
@@ -1,12 +1,16 @@
 package com.imooc.bootsell.controller;
 
+import com.imooc.bootsell.entity.ProductInfo;
 import com.imooc.bootsell.service.ProductService;
+import com.imooc.bootsell.utils.ResultVoUtil;
 import com.imooc.bootsell.utils.StaticUtil;
 import com.imooc.bootsell.vo.ResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -21,7 +25,8 @@ public class SellerProductController {
 
     @RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = StaticUtil.APPLICATION_JSON_UTF8_VALUE)
     public ResultVo findAll(){
-        return null;
+        List<ProductInfo> productInfoList = this.productService.findAll();
+        return ResultVoUtil.success(productInfoList);
     }
 
 }
